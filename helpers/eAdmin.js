56effb87bed2fc937c1d75f653b979644e6c4646
ErickLima <ericklimacosta29@gmail.com>
2023-04-23 18:54:19 -0300

    middlewares Verificar se o usuario e admin

--- helpers/eAdmin.js
+++ helpers/eAdmin.js
@@ -1,10 +1,16 @@
 module.exports = {
     eAdmin: (req, res, next) => {
-        if(req.isAuthenticated() && req.user.eAdmin === 1) {
-            return next();
-        } 
-
-        req.flash("error_msg", "Acesso Negado!");
-        res.redirect("/");
+      if (!req.isAuthenticated()) {
+        req.flash("error_msg", "Você precisa estar logado para acessar esta página.");
+        return res.redirect("/");
+      }
+  
+      if (req.user.eAdmin !== 1) {
+        req.flash("error_msg", "Você não tem permissão para acessar esta página.");
+        return res.redirect("/");
+      }
+  
+      return next();
     }
-}
+  };
+  
