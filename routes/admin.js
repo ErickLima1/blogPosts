55356796a990d06cef1eab21794724042f297fc6
ErickLima <ericklimacosta29@gmail.com>
2023-04-30 12:58:18 -0300

    admin.js rotas do admin atualizado, Adicionando rota do crud para adimnistrador

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,23 @@
 const express = require("express");
-const { route } = require("express/lib/application");
 const { type } = require("express/lib/response");
+
 const router = express.Router();
+const { route } = require("express/lib/application");
+
 const mongoose = require("mongoose");
+
 require("../models/Categoria");
 const Categoria = mongoose.model("categorias");
+
 require('../models/Postagem');
 const Postagem = mongoose.model("postagem");
+
+require("../models/Usuario");
+const Usuario = mongoose.model("usuarios");
 const {eAdmin} = require("../helpers/eAdmin");
+
+// const bcrypt = require("bcryptjs");
+// const passport = require("passport");
 
 
 router.get("/", eAdmin, (req, res) => {
@@ -20,48 +30,54 @@ router.get("/posts", eAdmin, (req, res) => {
 //Lista as Categorias salva no banco(vai aparecer na listagem do front)
 router.get("/categorias", eAdmin, (req, res) => {
     Categoria.find().lean().sort({date: 'desc'}).then((categorias) => {
+        categorias.forEach((categoria) => {
+            categoria.dataFormatada = new Date(categoria.date).toLocaleDateString('pt-BR');
+        });
         res.render("admin/categorias", {categorias: categorias});
-
     }).catch((err) => {
         req.flash("error_msg", "Houve um erro ao listar as categorias");
         res.redirect("/admin");
-    })
-})
+    });
+});
 
 router.post("/categorias/nova", eAdmin, (req, res) => {
     var erros = [];
+  
+    const campos = [
+      {nome: "nome", mensagem: "Nome Invalido", minimo: 4},
+      {nome: "slug", mensagem: "Slug Invalido", minimo: 0},
+    ];
+  
+    for (let campo of campos) {
+      if (!req.body[campo.nome] || typeof req.body[campo.nome] === undefined || req.body[campo.nome] === null) {
+        erros.push({texto: campo.mensagem});
 
-    if (!req.body.nome || typeof req.body.nome === undefined || req.body.nome === null) {
-        erros.push({texto: "Nome Invalido"});
-
-    } else if(req.body.nome.length < 4) {
-        erros.push({texto: "Nome da Categoria tem que ser mas de 5 caracteres"});
+      } else if (req.body[campo.nome].length < campo.minimo) {
+        erros.push({texto: `O campo ${campo.nome} deve ter pelo menos ${campo.minimo} caracteres`});
+      }
     }
+  
+    if (erros.length > 0) {
+      res.render("admin/addCategorias", {erros: erros});
 
-    if (!req.body.slug || typeof req.body.slug === undefined || req.body.slug === null) {
-        erros.push({texto: "Slug Invalido"});
+    } else {
 
+      const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug.replace(/ /g, "-"),
+      };
+
+      new Categoria(novaCategoria).save().then(() => {
+        // console.log("Categoria Salva");
+        req.flash("success_msg", "Categoria Criada");
+        res.redirect("/admin/categorias");
+
+      }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao salvar a categoria");
+        res.redirect("/admin");
+      });
     }
-
-    if(erros.length > 0) {
-        res.render("admin/addCategorias", {erros: erros});
-
-    }else{
-        const novaCategoria = {
-            nome: req.body.nome,
-            slug: req.body.slug
-        }
-        new Categoria(novaCategoria).save().then(() => {
-            console.log("Categoria Salva");
-            req.flash("success_msg", "Categoria Criada");
-            res.redirect("/admin/categorias");
-
-        }).catch((err) => {
-            req.flash("error_msg", "Houve um erro ao salvar a categoria");
-            res.redirect("/admin");
-        })
-    }
-})
+  });
 
 router.get("/categorias/edit/:id", eAdmin, (req, res) => {
     Categoria.findOne({_id:req.params.id}).lean().then((categorias) => {
@@ -92,7 +108,7 @@ router.post("/categorias/edit", eAdmin, (req, res) => {
     }else{
         Categoria.findOne({_id: req.body.id}).then((categorias) => {
             categorias.nome = req.body.nome;
-            categorias.slug = req.body.slug;
+            categorias.slug = req.body.slug.replace(/ /g, "-"),
             categorias.data = new Date;
             
             categorias.save().then(() => {
@@ -113,7 +129,7 @@ router.post("/categorias/edit", eAdmin, (req, res) => {
 
 router.post("/categorias/deletar", eAdmin, (req, res) => {
     Categoria.deleteOne({_id: req.body.id}).then(() => {
-        req.flash("error_msg", "Categoria Deletada");
+        req.flash("error_msg", "Categoria Excluida");
         res.redirect("/admin/categorias");
         
     }).catch((err) =>  {
@@ -125,11 +141,13 @@ router.post("/categorias/deletar", eAdmin, (req, res) => {
 router.get('/categorias/add', eAdmin, (req, res) => {
     res.render("admin/addCategorias");
 });
-//postagem
+//Lista as Postagem salva no banco(vai aparecer na listagem do front) 
 router.get("/postagem", eAdmin, (req, res) => {
-    Postagem.find().lean().populate("categoria").sort({_id: -1 }).then((postagem) => {
+    Postagem.find().lean().populate("categoria").sort({_id: -1}).then((postagem) => {
+        postagem.forEach((p) => {
+            p.dataFormatada = new Date(p.data).toLocaleDateString('pt-BR') //Colocando a data PT-BR
+        });
         res.render("admin/postagens", {postagem: postagem});
-
     }).catch((err) => {
         req.flash("error_msg", "Houve um Erro Inesperado");
         res.redirect("/admin");
@@ -169,7 +187,7 @@ router.post("/postagem/nova", eAdmin, (req, res) => {
     }else {
         const novaPostagem = {
             titulo: req.body.titulo,
-            slug: req.body.slug,
+            slug: req.body.slug.replace(/ /g, "-"),
             descricao: req.body.descricao,
             conteudo: req.body.conteudo,
             categoria: req.body.categoria
@@ -206,7 +224,7 @@ router.post("/postagem/edit", eAdmin, (req, res) => {
     var erro = [];
     Postagem.findOne({_id: req.body.id}).then((postagem) => {
         postagem.titulo = req.body.titulo;
-        postagem.slug = req.body.slug;
+        postagem.slug = req.body.slug.replace(/ /g, "-");
         postagem.descricao = req.body.descricao;
         postagem.conteudo = req.body.conteudo;
         postagem.categoria = req.body.categoria;
@@ -236,4 +254,75 @@ router.post("/postagem/deletar", eAdmin, (req, res) => {
     });
 });
  
+//rota do Curd(quantos usuarios tem cadastrado no sistema);
+router.get("/crud", eAdmin, (req, res) => {
+    Usuario.find().lean().sort({date: 'desc'}).then((usuario) => {
+        res.render("admin/crud", {usuario: usuario});
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro inesperado");
+        req.redirect("/");
+    });
+});
+
+//parte que postEdit
+router.get("/crud/edit/:id", eAdmin, (req, res) => {
+    Usuario.findOne({_id:req.params.id}).lean().then((usuario) => {
+        res.render("admin/editUsuarios", {usuario: usuario});
+    }).catch((err) => {
+        req.flash("error_msg", "Usuário Não Existe.");
+        res.redirect("/admin/crud");
+    });
+});
+
+router.post("/crud/edit", eAdmin, (req, res) => {
+    var erros = [];
+    const campos = [
+        {nome: "nome", mensagem: "Nome em Branco", campoBranco: ""},
+        {nome: "email", mensagem: "E-mail em Branco", campoBranco: ""},
+    ];
+
+    for(let campo of campos) {
+        if(!req.body[campo.nome] || typeof req.body[campo.nome] === undefined || req.body[campo.nome] === null) {
+            erros.push({texto: campo.mensagem});
+
+        } else if(req.body[campo.nome].trim() === campo.campoBranco.trim()) {
+            erros.push({texto: "O Campo " + campo.nome + " não pode ser vazio!"});
+        };
+    };
+
+    if(erros.length > 0) {
+        // console.log(erros)
+        res.render("admin/editUsuarios", {erros: erros});
+    }else{
+        Usuario.findOne({_id: req.body.id}).then((usuario) => {
+            usuario.nome = req.body.nome;
+            usuario.email = req.body.email;
+            usuario.eAdmin = req.body.eAdmin ? 1 : 0;
+            
+            usuario.save().then(() => {
+                req.flash("success_msg", "Usuário foi modificado com sucesso");
+                res.redirect("/admin/crud");
+            }).catch((err) => {
+                req.flash("error_msg", "Houve um erro ao editar o usuário");
+                res.redirect("/admin/crud");
+            });
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um Erro inesperado");
+            res.redirect("/admin/crud");
+        });
+    };
+
+});
+
+router.post("/crud/deletar", eAdmin, (req, res) => {
+    Usuario.deleteOne({_id: req.body.id}).then(() => {
+        req.flash("error_msg", "Usuario Deletado");
+        res.redirect("/admin/crud");
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro inesperado");
+        res.redirect("/")
+    });
+});
+
+
 module.exports = router;
